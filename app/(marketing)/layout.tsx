cdb294a9f689c1dd7a0a7b303465111ec6729d93
minhhieu2122000@gmail.com <minhhieu2122000@gmail.com>
2025-01-25 18:04:28 +0700

    deploy

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from '@/views/marketing/components/navbar'
 
 function MarketingLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="h-full dark:bg-[#1F1F1F]">
+		<div className="min-h-full dark:bg-[#1F1F1F]">
 			<Navbar />
 			<main className="h-full pt-40">{children}</main>
 		</div>
